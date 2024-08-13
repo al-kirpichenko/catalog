4a91647ea40349eb6e5551dd0044fb0ffba3aee2
al-kirpichenko <fuluganchik@yandex.ru>
2024-08-13 17:55:12 +0700

    add model and migration order

--- database/migrations/2024_08_13_105222_create_orders_table.php
+++ database/migrations/2024_08_13_105222_create_orders_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->decimal('price', 8, 2);
+            $table->decimal('total_price', 8, 2);
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('orders');
     }
 };
